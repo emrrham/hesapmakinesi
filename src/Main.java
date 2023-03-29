ad505ce395cd92ce75c0265bd854aa4891979709
emrrham <mremir255@gmail.com>
2023-03-29 17:27:03 +0300

    kullanıcıadı şifre programı

--- src/Main.java
+++ src/Main.java
@@ -2,40 +2,24 @@
 
     public class Main {
     public static void main(String[] args) {
-     int n1,n2,select;
-     Scanner input = new Scanner(System.in);
-        System.out.println("İlk Sayı ?");
-     n1 = input.nextInt();
+      String userName,password;
 
-        System.out.println("ikinci sayı ?");
-     n2 = input.nextInt();
-     System.out.println("1-Toplama\n2-çıkarma\n3-çarpma\n4-bölme\n5-atak");
-     System.out.print("Seçimin?");
-     select = input.nextInt();
-     System.out.println(select);
-        switch (select) {
-            case 1 :
-                System.out.println("Toplam : " +  (n1 + n2) );
+      Scanner inp = new Scanner(System.in);
+      System.out.print("Gulag Kullanıcı adı giriniz : ");
+      userName = inp.nextLine();
 
-                break;
-            case 2:
-                System.out.print("Çıkarma : " + (n1 - n2));
+      System.out.print("Gulag Şifresi Giriniz : ");
+      password = inp.nextLine();
+      if(userName.equals("emrrham") && password.equals("akif") ){
+          System.out.println("Bilgileriniz doğru! добро пожаловать солдат!!! ");
 
-                break;
-            case 3:
-                System.out.println("Çarpım : " + (n1 * n2));
-                break;
-            case 4:
-                System.out.println("Bölüm : " + (n1/n2));
-               break;
-            case 5:
-                System.out.println("ATAK BAŞARAMADIN");
-                break;
-            default:
-                System.out.println("YANLIŞ GİRDİN YA ZATEN 5 TANE VAR AMK ");
-                break;
+      }
+else {
+    System.out.println("Bilgileriniz Yanlış  kürek mahkûmları kampına aktarılıyorsunuz...");
+      }
+
+            }
 
 
     }
-    }
-    }
+
