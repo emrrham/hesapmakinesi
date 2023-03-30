412caf7c7a943d0e0f0fecf96e3b152e3cdbf4c8
emrrham <mremir255@gmail.com>
2023-03-30 16:47:22 +0300

    Sıralamahesap

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,36 @@
-   import java.util.Scanner;
-
-    public class Main {
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import java.util.Scanner;
+public class Main {
     public static void main(String[] args) {
-        int sıcaklık;
-        Scanner input = new Scanner(System.in);
-                System.out.println("Sıcaklık giriniz : (°C cinsinden)");
-                sıcaklık = input.nextInt();
-        if (sıcaklık < 5){
-            System.out.println("PC oynamaya devam..");
-            
-        } else if (sıcaklık >= 5 && sıcaklık<= 25 ) {
-            if (sıcaklık<=15){System.out.println("Bahçeye git");
-            
-        }if (sıcaklık >= 10){
-                System.out.println("Pikniğe git.");
-            } else if (sıcaklık>25) {
-                System.out.println("Yüzmeye git!");
-            }else
-                System.out.println("akifi görmeye gidebilirsin.");
-                
+        Scanner girdi = new Scanner(System.in);
+        int a, b, c;
+
+        System.out.print("1-Sayıyı Giriniz: ");
+        a = girdi.nextInt();
+        System.out.print("2-Sayıyı Giriniz: ");
+        b = girdi.nextInt();
+        System.out.print("3-Sayıyı Giriniz: ");
+        c = girdi.nextInt();
+        if ((a > b) && (a > c)) {
+            if (b > c) {
+                System.out.println("a>b>c");
+            }else {
+                    System.out.println("a > c > b");
+            }
+        }else if ((b>a) && (b>c)){
+            if (a>c){
+                System.out.println("b > a > c");
+            }else {
+                System.out.println("b > c > a");
+            }
+        } else if ((c>a) && (c>b)) {
+            if (a > b) {
+                System.out.println("c > a > b ");
+            }else {
+                System.out.println("c > b > a");
             }
 
 
         }
-
-
     }
-
+}
